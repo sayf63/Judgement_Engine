cd674f491827f8cbfcda6a4820d75de2e39337f1
Gabriel-Welsh <gjw4492@gmail.com>
2026-02-15 16:02:30 -0600

    player transitioning to using finite state machine :)

--- judgement-engine/Entities/Player/MousePlayer.gd
+++ judgement-engine/Entities/Player/MousePlayer.gd
@@ -3,15 +3,13 @@ extends CharacterBody2D
 
 @export var SPEED := 400.0
 @export var DASH_MULTIPLIER := 2.0
-@export var DASH_COOLDOWN := 1.0
+@export var DASH_COOLDOWN := 2.0
 @export var DASH_DURATION := 1.0
 @export var WEAPON_DISTANCE := 100.0
 @export var WEAPON_ROTATION_SPEED := 10.0
 
 @onready var Weapon = $PlayerWeapon
 
-var dash_timer := 0.0
-var is_dashing := false
 var dash_is_cooldown := false
 
 const ACTIONS = {
@@ -22,18 +20,63 @@ const ACTIONS = {
 	Dash = "dash"
 }
 
+enum STATES {
+	MOVING,
+	IDLE,
+	DASHING
+}
+
+var state := STATES.IDLE
+
+var cooldowns: Dictionary = {
+	ACTIONS.Dash: DASH_COOLDOWN
+}
+
+func _is_on_cooldown(action: String) -> bool:
+	return cooldowns[action] != 0
+
 var previous_direction := Vector2.ZERO
 
+func _init() -> void:
+	for key in cooldowns:
+		cooldowns[key] = 0
+
+func set_state(new_state: STATES) -> void:
+	var previous_state = state
+	state = new_state
+	
+	match state:
+		STATES.MOVING:
+			pass
+		STATES.IDLE:
+			pass
+		STATES.DASHING:
+			cooldowns[ACTIONS.Dash] = DASH_COOLDOWN
+			get_tree().create_timer(DASH_DURATION).timeout.connect(
+				func(): state = STATES.MOVING
+			)
+		_:
+			pass
+
 func _physics_process(delta: float) -> void:
+	for key in cooldowns:
+		cooldowns[key] = max(cooldowns[key] - delta, 0.0)
+	
 	var mouse_position = get_global_mouse_position()
 	
 	handle_mouse_movement(delta, mouse_position)
 	
-	handle_dash(delta)
+	if Input.is_action_just_pressed(ACTIONS.Dash) and !_is_on_cooldown(ACTIONS.Dash) and state != STATES.DASHING:
+		set_state(STATES.DASHING)
 	
 	handle_weapon_position(delta, mouse_position)
 	
 	move_and_slide()
+	if velocity == Vector2.ZERO:
+		set_state(STATES.IDLE)
+	elif state != STATES.DASHING:
+		set_state(STATES.MOVING)
+	print(state)
 
 func handle_weapon_position(delta: float, mouse_position: Vector2):
 	var weapon_desired_angle := global_position.angle_to_point(mouse_position)
@@ -57,7 +100,7 @@ func handle_mouse_movement(delta, mouse_position: Vector2):
 	var t := smoothstep(0.0, slow_radius, distance)
 	target_speed_multiplier = lerp(min_speed_factor, 1.0, t)
 	
-	if is_dashing:
+	if state == STATES.DASHING:
 		target_speed_multiplier += DASH_MULTIPLIER-1
 	
 	var direction = position.direction_to(mouse_position)
@@ -65,20 +108,3 @@ func handle_mouse_movement(delta, mouse_position: Vector2):
 		move_toward(velocity.x, direction.x * SPEED * target_speed_multiplier * delta*50, SPEED),
 		move_toward(velocity.y, direction.y * SPEED * target_speed_multiplier * delta*50, SPEED)
 	)
-
-func handle_dash(delta: float) -> void:
-	if Input.is_action_just_pressed(ACTIONS.Dash) && !dash_is_cooldown:
-		is_dashing = true
-	
-	if is_dashing:
-		dash_timer += delta
-		if dash_timer > DASH_DURATION:
-			dash_timer = 0
-			is_dashing = false
-			dash_is_cooldown = true
-	
-	if dash_is_cooldown:
-		dash_timer += delta
-		if dash_timer > DASH_COOLDOWN:
-			dash_is_cooldown = false
-			dash_timer = 0
